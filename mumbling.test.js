d7865024e840ca635d1632bf08cef2fec565b001
Scott Edwards <scotteza@gmail.com>
2020-04-24 14:40:58 +0100

    Implement 3-letter case

--- mumbling.test.js
+++ mumbling.test.js
@@ -1,44 +1,52 @@
-function mumble_letters (letters){
-    if(letters.length === 2){
-        return `${letters[0].toUpperCase()}-${letters[1].toUpperCase()}${letters[1].toLowerCase()}`
-    }
-    
-    return letters.toUpperCase()
+function mumble_letters(letters) {
+  if (letters.length === 2) {
+    return `${letters[0].toUpperCase()}-${letters[1].toUpperCase()}${letters[1].toLowerCase()}`;
+  } else if (letters.length === 3) {
+    return `${letters[0].toUpperCase()}-${letters[1].toUpperCase()}${letters[1].toLowerCase()}-${letters[2].toUpperCase()}${letters[2].toLowerCase()}${letters[2].toLowerCase()}`;
   }
-    
-    test('Empty test', () => {
-        expect(true).toBe(true)
-    })
-    test('Mumbles nothing', () => {
-      expect(mumble_letters('')).toBe('')
-    })
-    test('Mumbles a single letter "A"', () => {
-      expect(mumble_letters('a')).toBe('A')
-    })
-    test('Mumbles a single letter "B"', () => {
-        expect(mumble_letters('b')).toBe('B')
-    })
-    test('Mumbles a capital letter "A"', () => {
-        expect(mumble_letters('A')).toBe('A')
-    })
-    test('Mumbles a two letter word', () => {
-        expect(mumble_letters('aa')).toBe('A-Aa')
-    })
-    test('Mumbles a two letter word "bb"', () => {
-        expect(mumble_letters('bb')).toBe('B-Bb')
-    })
-    test('Mumbles a two letter word "cc"', () => {
-        expect(mumble_letters('cc')).toBe('C-Cc')
-    })
-    test('Mumbles a two letter word "CC"', () => {
-        expect(mumble_letters('CC')).toBe('C-Cc')
-    })
-    test('Mumbles a two letter word "ab"', () => {
-        expect(mumble_letters('ab')).toBe('A-Bb')
-    })
-    test('Mumbles a two letter word "AB"', () => {
-        expect(mumble_letters('AB')).toBe('A-Bb')
-    })
-    test('Mumbles a three letter word "aaa"', () => {
-        expect(mumble_letters('aaa')).toBe('A-Aa-Aaa')
-    })
+
+  return letters.toUpperCase();
+}
+
+test("Empty test", () => {
+  expect(true).toBe(true);
+});
+test("Mumbles nothing", () => {
+  expect(mumble_letters("")).toBe("");
+});
+test('Mumbles a single letter "A"', () => {
+  expect(mumble_letters("a")).toBe("A");
+});
+test('Mumbles a single letter "B"', () => {
+  expect(mumble_letters("b")).toBe("B");
+});
+test('Mumbles a capital letter "A"', () => {
+  expect(mumble_letters("A")).toBe("A");
+});
+test("Mumbles a two letter word", () => {
+  expect(mumble_letters("aa")).toBe("A-Aa");
+});
+test('Mumbles a two letter word "bb"', () => {
+  expect(mumble_letters("bb")).toBe("B-Bb");
+});
+test('Mumbles a two letter word "cc"', () => {
+  expect(mumble_letters("cc")).toBe("C-Cc");
+});
+test('Mumbles a two letter word "CC"', () => {
+  expect(mumble_letters("CC")).toBe("C-Cc");
+});
+test('Mumbles a two letter word "ab"', () => {
+  expect(mumble_letters("ab")).toBe("A-Bb");
+});
+test('Mumbles a two letter word "AB"', () => {
+  expect(mumble_letters("AB")).toBe("A-Bb");
+});
+test('Mumbles a three letter word "aaa"', () => {
+  expect(mumble_letters("aaa")).toBe("A-Aa-Aaa");
+});
+test('Mumbles a three letter word "abc"', () => {
+  expect(mumble_letters("abc")).toBe("A-Bb-Ccc");
+});
+test('Mumbles a three letter word "AbC"', () => {
+  expect(mumble_letters("AbC")).toBe("A-Bb-Ccc");
+});
